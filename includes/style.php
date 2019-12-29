304bb77ec18a48b2b775f2c21f79c70298e8aeab
shadab <ks615044@gmail.com>
2019-12-29 11:55:03 -0800

    added file register

---
+++ includes/style.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+  folder instead of downloading all of them to reduce the load. -->
+  <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet">
+    <style type="text/css">
+      
+      *{
+        padding: 0px;
+        margin: 0px;
+        box-sizing: border-box; 
+      }
+    </style>
+
+</head>
+<body>
+    
+</body>
+</html>
